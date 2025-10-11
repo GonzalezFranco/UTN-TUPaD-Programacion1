f17f5a10481428b893635c989d45cddeda91bfa1
GonzalezFranco <francoa.gonzalez@tupad.utn.edu.ar>
2025-10-11 00:42:40 -0300

    Avance Unidad 7

---
+++ Unidad.7-Estructuras-Datos-Complejos/T.P.-Estructuras-Datos-Complejos.py
@@ -0,0 +1,24 @@
+#Ejercicio1 --------------------------------------------------------------------------------
+precios_frutas = {'Banana': 1200, 'Anana': 2500, 'Melon': 3000, 'Uva':1450}                 #|
+                                                                                            #|
+precios_frutas ["Naranja"] = 1200                                                           #|
+precios_frutas ["Manzana"] = 1200                                                           #| 
+precios_frutas ["Pera"] = 1200                                                              #|
+                                                                                            #|
+print(precios_frutas,"\n")                                                                       #|
+                                                                                            #|
+#---------------                                                                            #|
+#Ejercicio2                                                                                  |
+precios_frutas ["Banana"] = 1330                                                            #|
+precios_frutas ["Manzana"] = 1700                                                           #|
+precios_frutas ["Melon"] = 2800                                                             #|
+                                                                                            #|
+print(precios_frutas,"\n")                                                                       #|
+                                                                                            #|
+#---------------                                                                             #|
+#Ejercicio3                                                                                  |
+frutas = list(precios_frutas.keys())                                                        #|
+print (frutas)                                                                              #|
+                                                                                            #|
+#--------------------------------------------------------------------------------------------|
+#Ejercicio3                                                                                                
