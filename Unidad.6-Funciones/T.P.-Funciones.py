e074700a35fd1bd1845532c036074bd8f4c933af
GonzalezFranco <francoa.gonzalez@tupad.utn.edu.ar>
2025-10-10 22:22:24 -0300

    T.P.-Funciones Finalizado

--- Unidad.6-Funciones/T.P.-Funciones.py
+++ Unidad.6-Funciones/T.P.-Funciones.py
@@ -1,3 +1,6 @@
+#Librerias
+import math
+
 #------------------------------------------------------
 #Eejercicio1
 def imprimir_hola_mundo():
@@ -30,4 +33,89 @@ informacion_personal(nombre,apellido,edad,domicilio)
 
 #------------------------------------------------------
 #Ejercicio4
-pass
+def calcular_area_circulo(radio):
+    area = math.pi * (radio ** 2) 
+    return area
+
+def calcular_perimetro_circulo(radio):
+    perimetro = 2 * math.pi * radio
+    return perimetro
+
+radio = float(input("Por favor ingrese el radio del circulo al cual desea sacar el area y perimetro: "))
+
+print(f"El area del circulo es: {calcular_area_circulo(radio)}")
+print(f"El perimetro del circulo es: {calcular_perimetro_circulo(radio)}")
+
+#------------------------------------------------------
+#Ejercicio5
+
+def segundos_a_horas(segundos):
+    minutos = segundos / 60
+    horas = minutos / 60
+    return horas
+
+segundos = int(input("Por favor ingrese la cantidad de segundos que desea pasar a horas: "))
+
+print(f"Los segundos ingresados son: {segundos_a_horas(segundos)} Hrs.")
+
+#------------------------------------------------------
+#Ejercicio6
+
+def tabla_multiplicar(numero):
+    for i in range (11):
+        print (f"{numero} * {i} = {numero * i}")
+               
+numero = int(input("Por favor ingrese el numero del cual quiere saber su tabla de multiplicar: "))
+
+tabla_multiplicar(numero)
+
+#------------------------------------------------------
+#Ejercicio7
+def operaciones_basicas(a,b):
+    print(f"La suma de dichos numeros es: {a + b}")
+    print(f"La resta de dichos numeros es: {a - b}")
+    print(f"La multiplicacion de dichos numeros es: {a * b}")
+    print(f"La division de dichos numeros es: {a / b}")
+    
+
+a = int(input("Por favor ingrese el primer digito: "))
+b = int(input("Por favor ingrese el segundo digito: "))
+
+operaciones_basicas(a,b)
+
+#------------------------------------------------------
+#Ejercicio8
+
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    imc = round(imc,2)
+    return imc
+
+peso = float(input("Por favor ingrese su peso en Kg. : "))
+altura = float(input("Por favor ingrese su altura en Metros: "))
+
+print(f"Su indice de masa corporal es: {calcular_imc(peso, altura)}")
+
+#-------------------------------------------------------
+#Ejercicio9
+
+def celsius_a_fahrenheit(celsius):
+    fahrenheit = celsius * 1.8 + 32
+    return fahrenheit
+
+celsius = float(input("Por favor ingrese la temperatura actual en grados celcius: "))
+
+print(f"La temperatura en grados fahrenheit es: {celsius_a_fahrenheit(celsius)}")
+
+#-------------------------------------------------------
+#Ejercicio10
+
+def calcular_promedio(a, b, c):
+    promedio = (a + b + c) / 3
+    return promedio
+
+a = float(input("Por favor ingrese el primer numero: "))
+b = float(input("Por favor ingrese el segundo numero: "))
+c = float(input("Por favor ingrese el tercer numero: "))
+
+print(f"El promedio de los tres numeros es: {calcular_promedio(a,b,c)}")
